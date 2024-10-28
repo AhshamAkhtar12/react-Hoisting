61ed78c8b834f93d839759ca40296fa591e4315d
AhshamAkhtar12 <ahshamakhtar43@gmail.com>
2024-10-28 12:43:57 +0500

    create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  console.log('hello world');
+  
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="div">
+
+    <h1 style={{color:'black'}}>Ahsham</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
